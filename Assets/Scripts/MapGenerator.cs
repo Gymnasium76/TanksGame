0cd9db1e329d05642f2ecf28618b46781613d762
briz777 <onaberezhnyy@gmail.com>
2020-11-21 17:07:12 +0300

    INITIALIZE COMMIT

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public int width;
+    public int height;
+    public int distance;
+    public int space;
+
+    GameObject 
     // Start is called before the first frame update
     void Start()
     {
