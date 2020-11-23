4efbcb168b40c87927c89d5cd3bdc7e0a4749667
briz777 <onaberezhnyy@gmail.com>
2020-11-23 17:40:58 +0300

    Update

--- Assets/Scripts/TankScript.cs
+++ Assets/Scripts/TankScript.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
-public class Tanks : MonoBehaviour
+public class TankScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject W;
+    public GameObject A;
+    public GameObject S;
+    public GameObject D;
+    public GameObject tank;
+    float speed = 0.3f;
+
+    private void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(CrossPlatformInputManager.GetButtonDown("UpBtn"))
+        {
+            tank.transform.Translate(0, speed /** Time.deltaTime*/, 0);
+            W.SetActive(true);
+            A.SetActive(false);
+            S.SetActive(false);
+            D.SetActive(false);
+        }
+        else if (CrossPlatformInputManager.GetButtonDown("DownBtn"))
+        {
+            tank.transform.Translate(0, -speed /** Time.deltaTime*/, 0);
+            W.SetActive(false);
+            A.SetActive(false);
+            S.SetActive(true);
+            D.SetActive(false);
+        }
+        else if (CrossPlatformInputManager.GetButtonDown("LeftBtn"))
+        {
+            tank.transform.Translate(-speed /** Time.deltaTime*/, 0 , 0);
+            W.SetActive(false);
+            A.SetActive(true);
+            S.SetActive(false);
+            D.SetActive(false);
+        }
+        else if (CrossPlatformInputManager.GetButtonDown("RightBtn"))
+        {
+            tank.transform.Translate(speed /** Time.deltaTime*/,0,  0);
+            W.SetActive(false);
+            A.SetActive(false);
+            S.SetActive(false);
+            D.SetActive(true);
+        }
     }
 }
